14a39faf5a65b01fc77a298fc48c454f3e5ac7e0
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-02-06 21:13:39 +0900

    fix : add year, month, day

--- src/main/java/com/selab/todo/dto/request/DiaryRegisterRequest.java
+++ src/main/java/com/selab/todo/dto/request/DiaryRegisterRequest.java
@@ -3,8 +3,11 @@ package com.selab.todo.dto.request;
 import lombok.Data;
 
 @Data
-public class TodoRegisterRequest {
+public class DiaryRegisterRequest {
     private final String title;
     private final String content;
     private final String feel;
+    private int year;
+    private int month;
+    private int day;
 }
