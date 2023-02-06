660ea95ae7ef73e974d1d7417728a9f76f07f2fd
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-02-06 21:16:38 +0900

    fix : delete test

---
+++ src/test/java/com/selab/todo/SelabDiaryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.selab.todo;
+
+import com.selab.todo.dto.response.DiaryResponse;
+import com.selab.todo.repository.DiaryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+class SelabDiaryApplicationTests {
+
+}
