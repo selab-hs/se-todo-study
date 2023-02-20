bdccfdd5b52048965e5a3697ea6248c7719a8df5
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-02-20 15:04:47 +0900

    fix : ApiOperation 수정

--- src/main/java/com/selab/todo/controller/SearchController.java
+++ src/main/java/com/selab/todo/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.selab.todo.controller;
 
+import com.selab.todo.common.dto.PageDto;
 import com.selab.todo.dto.request.search.MonthSearchRequest;
 import com.selab.todo.dto.request.search.SearchRequest;
 import com.selab.todo.dto.request.search.YearSearchRequest;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,28 +28,28 @@ public class SearchController {
     private final DataSearchService dataSearchService;
     private final DiaryService diaryService;
 
-    @ApiOperation("Month 별로 조회하기")
+    @ApiOperation("Month 별로 검색")
     @GetMapping(value = "/month",consumes = MediaType.APPLICATION_JSON_VALUE)
     public Page<DiaryResponse> searchMonth(@PageableDefault Pageable pageable, MonthSearchRequest request) {
         Page<DiaryResponse> diaryResponses = diaryService.getAll(pageable);
         return dataSearchService.searchMonth(diaryResponses, request.getMonth());
     }
 
-    @ApiOperation("Year 별로 조회하기")
+    @ApiOperation("Year 별로 검색")
     @GetMapping(value = "/year",consumes = MediaType.APPLICATION_JSON_VALUE)
     public Page<DiaryResponse> searchYear(@PageableDefault Pageable pageable, YearSearchRequest request) {
         Page<DiaryResponse> diaryResponses = diaryService.getAll(pageable);
         return dataSearchService.searchYear(diaryResponses, request.getYear());
     }
 
-    @ApiOperation("Year, Month, Day 조회")
+    @ApiOperation("Year, Month, Day 검색")
     @GetMapping(value = "/year-month-day",consumes = MediaType.APPLICATION_JSON_VALUE)
     public Page<DiaryResponse> searchYear(@PageableDefault Pageable pageable, SearchRequest request) {
         Page<DiaryResponse> diaryResponses = diaryService.getAll(pageable);
         return dataSearchService.search(diaryResponses, request);
     }
 
-    @ApiOperation("Feeling 조회")
+    @ApiOperation("Feeling 검색")
     @GetMapping(value = "/feel")
     public Page<DiaryResponse> searchFeeling(@PageableDefault Pageable pageable, FeelingSearchRequest request){
         Page<DiaryResponse> diaryResponses = diaryService.getAll(pageable);
