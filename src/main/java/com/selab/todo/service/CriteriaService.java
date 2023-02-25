067294b0cf46c2a9157e0008d33f5dcb61bdfb34
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-02-25 20:52:13 +0900

    update : feelingSearch

--- src/main/java/com/selab/todo/service/CriteriaService.java
+++ src/main/java/com/selab/todo/service/CriteriaService.java
@@ -1,8 +1,12 @@
 package com.selab.todo.service;
 
+import com.selab.todo.dto.response.DiaryResponse;
 import com.selab.todo.entity.Diary;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,41 +14,55 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.time.DayOfWeek;
 import java.time.Month;
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CriteriaService {
 
+    private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
+
     @Transactional
-    public void get(){
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
-
+    public Page<DiaryResponse> searchFeel(String feeling){
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
+        CriteriaQuery<Diary> criteriaQuery = criteriaBuilder.createQuery(Diary.class);
+        Root<Diary> root = criteriaQuery.from(Diary.class);
 
-        try{
-            Diary diary = new Diary(
-                    "Test",
-                    "test",
-                    "Good",
-                    Year.now(),
-                    Month.of(2),
-                    DayOfWeek.MONDAY
-            );
+        Predicate feelEqual = criteriaBuilder.equal(root.get("feeling"), feeling);
 
-            entityManager.persist(diary);
-        }catch (Exception e){
-            transaction.rollback();
-        }finally {
-            entityManager.close();
-        }
+        javax.persistence.criteria.Order idDesc = criteriaBuilder.desc(root.get("id"));
 
-        entityManagerFactory.close();
+        criteriaQuery.select(root)
+                .where(feelEqual)
+                .orderBy(idDesc);
+
+        List<Diary> result = entityManager.createQuery(criteriaQuery).getResultList();
+
+        List<DiaryResponse> mapping = new ArrayList<>();
+
+        result.stream()
+                .forEach(a->{
+                    mapping.add(DiaryResponse.from(a));
+                });
+
+        return makePage(mapping);
+    }
+
+    private PageImpl<DiaryResponse> makePage(List<DiaryResponse> process) {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        int start = (int) pageRequest.getOffset();
+        int end = Math.min((start + pageRequest.getPageSize()), process.size());
+        return new PageImpl<>(process.subList(start, end), pageRequest, process.size());
     }
 }
