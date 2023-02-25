e2f77e909ad36129610972b05a518d46b4a4e0ec
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-02-25 20:51:30 +0900

    fix : GenerateValue

--- src/main/java/com/selab/todo/entity/Feeling.java
+++ src/main/java/com/selab/todo/entity/Feeling.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -17,6 +14,7 @@ import javax.persistence.Table;
 @AllArgsConstructor
 public class Feeling {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
