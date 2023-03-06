0976e814782ec248308a770cbbf82cb5547d2b15
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-03-06 09:46:41 +0900

    update : JpaSpecificationExecutor

--- src/main/java/com/selab/todo/repository/DiaryRepository.java
+++ src/main/java/com/selab/todo/repository/DiaryRepository.java
@@ -2,6 +2,7 @@ package com.selab.todo.repository;
 
 import com.selab.todo.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
+public interface DiaryRepository extends JpaRepository<Diary, Long>, JpaSpecificationExecutor<Diary> {
 
     //월별 삭제 쿼리
     @Query(
